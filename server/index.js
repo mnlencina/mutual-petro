d30438488b560c8f6e2cca7b9135a2037ae39403
joseDev <josemptj@gmail.com>
2023-11-10 10:58:36 -0300

    config server

---
+++ server/index.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+const server = require("./src/app");
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, () => {
+  console.log(`Server on PORT  ${PORT}`);
+});
